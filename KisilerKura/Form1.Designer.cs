3058d1122be68be293a599d86e11586b7bee51e9
Ali <Ali@DESKTOP-128K6P6>
2021-11-02 09:27:28 +0300

    Add project files.

---
+++ KisilerKura/Form1.Designer.cs
@@ -0,0 +1,206 @@
+﻿
+namespace KisilerKura
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.label1 = new System.Windows.Forms.Label();
+            this.lblSansli = new System.Windows.Forms.Label();
+            this.btnEkle = new System.Windows.Forms.Button();
+            this.btnKaristir = new System.Windows.Forms.Button();
+            this.lstKisiler = new System.Windows.Forms.ListBox();
+            this.txtAd = new System.Windows.Forms.TextBox();
+            this.btnSirala = new System.Windows.Forms.Button();
+            this.btnTop = new System.Windows.Forms.Button();
+            this.btnUp = new System.Windows.Forms.Button();
+            this.btnDown = new System.Windows.Forms.Button();
+            this.btnBottom = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(36, 18);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(44, 24);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Ad :";
+            // 
+            // lblSansli
+            // 
+            this.lblSansli.BackColor = System.Drawing.Color.Black;
+            this.lblSansli.Font = new System.Drawing.Font("Microsoft Sans Serif", 25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lblSansli.ForeColor = System.Drawing.Color.White;
+            this.lblSansli.Location = new System.Drawing.Point(360, 53);
+            this.lblSansli.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblSansli.Name = "lblSansli";
+            this.lblSansli.Size = new System.Drawing.Size(281, 268);
+            this.lblSansli.TabIndex = 1;
+            this.lblSansli.Text = "?";
+            this.lblSansli.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // btnEkle
+            // 
+            this.btnEkle.Location = new System.Drawing.Point(221, 13);
+            this.btnEkle.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.btnEkle.Name = "btnEkle";
+            this.btnEkle.Size = new System.Drawing.Size(94, 32);
+            this.btnEkle.TabIndex = 2;
+            this.btnEkle.Text = "Ekle";
+            this.btnEkle.UseVisualStyleBackColor = true;
+            this.btnEkle.Click += new System.EventHandler(this.btnEkle_Click);
+            // 
+            // btnKaristir
+            // 
+            this.btnKaristir.Location = new System.Drawing.Point(221, 329);
+            this.btnKaristir.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.btnKaristir.Name = "btnKaristir";
+            this.btnKaristir.Size = new System.Drawing.Size(94, 32);
+            this.btnKaristir.TabIndex = 3;
+            this.btnKaristir.Text = "Karıştır";
+            this.btnKaristir.UseVisualStyleBackColor = true;
+            this.btnKaristir.Click += new System.EventHandler(this.btnKaristir_Click);
+            // 
+            // lstKisiler
+            // 
+            this.lstKisiler.FormattingEnabled = true;
+            this.lstKisiler.ItemHeight = 22;
+            this.lstKisiler.Location = new System.Drawing.Point(12, 53);
+            this.lstKisiler.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.lstKisiler.Name = "lstKisiler";
+            this.lstKisiler.Size = new System.Drawing.Size(302, 268);
+            this.lstKisiler.TabIndex = 4;
+            this.lstKisiler.KeyDown += new System.Windows.Forms.KeyEventHandler(this.lstKisiler_KeyDown);
+            // 
+            // txtAd
+            // 
+            this.txtAd.Location = new System.Drawing.Point(88, 15);
+            this.txtAd.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.txtAd.Name = "txtAd";
+            this.txtAd.Size = new System.Drawing.Size(124, 28);
+            this.txtAd.TabIndex = 5;
+            this.txtAd.KeyDown += new System.Windows.Forms.KeyEventHandler(this.txtAd_KeyDown);
+            // 
+            // btnSirala
+            // 
+            this.btnSirala.Location = new System.Drawing.Point(12, 328);
+            this.btnSirala.Name = "btnSirala";
+            this.btnSirala.Size = new System.Drawing.Size(91, 33);
+            this.btnSirala.TabIndex = 6;
+            this.btnSirala.Text = "Sırala";
+            this.btnSirala.UseVisualStyleBackColor = true;
+            this.btnSirala.Click += new System.EventHandler(this.btnSirala_Click);
+            // 
+            // btnTop
+            // 
+            this.btnTop.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("btnTop.BackgroundImage")));
+            this.btnTop.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.btnTop.Location = new System.Drawing.Point(321, 122);
+            this.btnTop.Name = "btnTop";
+            this.btnTop.Size = new System.Drawing.Size(32, 32);
+            this.btnTop.TabIndex = 7;
+            this.btnTop.UseVisualStyleBackColor = true;
+            this.btnTop.Click += new System.EventHandler(this.btnTop_Click);
+            // 
+            // btnUp
+            // 
+            this.btnUp.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("btnUp.BackgroundImage")));
+            this.btnUp.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.btnUp.Location = new System.Drawing.Point(321, 160);
+            this.btnUp.Name = "btnUp";
+            this.btnUp.Size = new System.Drawing.Size(32, 32);
+            this.btnUp.TabIndex = 7;
+            this.btnUp.UseVisualStyleBackColor = true;
+            this.btnUp.Click += new System.EventHandler(this.btnUp_Click);
+            // 
+            // btnDown
+            // 
+            this.btnDown.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("btnDown.BackgroundImage")));
+            this.btnDown.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.btnDown.Location = new System.Drawing.Point(321, 198);
+            this.btnDown.Name = "btnDown";
+            this.btnDown.Size = new System.Drawing.Size(32, 32);
+            this.btnDown.TabIndex = 7;
+            this.btnDown.UseVisualStyleBackColor = true;
+            this.btnDown.Click += new System.EventHandler(this.btnDown_Click);
+            // 
+            // btnBottom
+            // 
+            this.btnBottom.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("btnBottom.BackgroundImage")));
+            this.btnBottom.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.btnBottom.Location = new System.Drawing.Point(321, 236);
+            this.btnBottom.Name = "btnBottom";
+            this.btnBottom.Size = new System.Drawing.Size(32, 32);
+            this.btnBottom.TabIndex = 7;
+            this.btnBottom.UseVisualStyleBackColor = true;
+            this.btnBottom.Click += new System.EventHandler(this.btnBottom_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 22F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(654, 385);
+            this.Controls.Add(this.btnBottom);
+            this.Controls.Add(this.btnDown);
+            this.Controls.Add(this.btnUp);
+            this.Controls.Add(this.btnTop);
+            this.Controls.Add(this.btnSirala);
+            this.Controls.Add(this.txtAd);
+            this.Controls.Add(this.lstKisiler);
+            this.Controls.Add(this.btnKaristir);
+            this.Controls.Add(this.btnEkle);
+            this.Controls.Add(this.lblSansli);
+            this.Controls.Add(this.label1);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Name = "Form1";
+            this.Text = "Kişiler Kura";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label lblSansli;
+        private System.Windows.Forms.Button btnEkle;
+        private System.Windows.Forms.Button btnKaristir;
+        private System.Windows.Forms.ListBox lstKisiler;
+        private System.Windows.Forms.TextBox txtAd;
+        private System.Windows.Forms.Button btnSirala;
+        private System.Windows.Forms.Button btnTop;
+        private System.Windows.Forms.Button btnUp;
+        private System.Windows.Forms.Button btnDown;
+        private System.Windows.Forms.Button btnBottom;
+    }
+}
+
---
+++ KisilerKura/Form1.cs
@@ -0,0 +1,206 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace KisilerKura
+{
+    public partial class Form1 : Form
+    {
+        List<string> kisiler;
+
+        // "Ali" , "Burak" , "Hande" , "Ömer" , "Aysegül" , "Ezgi" , "Seda" , "Ugurcan" , "Kudret" , "Arzu" , "Koray" , "Bilge" , "Bilgehan" , "Serkan" , "Emre" , "Tugba"
+        public Form1()
+        {
+            InitializeComponent();
+            VerileriOku();//başlarken çıkışta kaydettiği .txt den verileri çektik
+            KisileriListele();//mevcut liste vasra .txt içinde onları göstermesi için
+        }
+
+        private void VerileriOku()
+        {
+            try
+            {
+                string[] dizi = File.ReadAllLines("veri.txt");//.txt dosyasından listeyi çekmesi için
+                kisiler = dizi.ToList();//kisiler.AddRange(dizi); dosyadan gelen diziyi listeye attık
+            }
+            catch (Exception)
+            {
+
+                kisiler = new List<string>();//olurda .txt dosyası silinmişse diye boş liste oluşturup bunu kullandırdık
+            }
+        }
+
+        private void KisileriListele()
+        {
+            lstKisiler.Items.Clear();//alt alta eklemesin diye temizleyip listeyi tekrar yazdırıyoruz
+            foreach (string kisi in kisiler)
+            {
+                lstKisiler.Items.Add(kisi);
+            }
+        }
+
+        private void btnEkle_Click(object sender, EventArgs e)
+        {
+            string ad = txtAd.Text.Trim();
+
+            if (ad == string.Empty)//eğer kutu boşsa bir şey yapma eventi bitir demek için
+                return;
+            kisiler.Add(ad);
+            txtAd.Clear();//ekleme bitince kutuyu temizlemesi için
+            KisileriListele();//gösterilen listeyi anlık ekrana yansıtmak için yeniledik
+            txtAd.Focus();//mouse u bu controlün üstüne çeker(tekrar yazınca direk algılaması için)
+        }
+
+        private void btnSirala_Click(object sender, EventArgs e)
+        {
+            //lstKisiler.Sorted = true;
+
+            kisiler.Sort();
+            KisileriListele();
+        }
+
+        private void btnKaristir_Click(object sender, EventArgs e)
+        {
+            //ListBox.ObjectCollection list = lstKisiler.Items;
+            //Random rng = new Random();
+            //int n = list.Count;
+            //while (n > 1)
+            //{
+            //    n--;
+            //    int k = rnd.Next(n + 1);
+            //    string value = (string)list[k];
+            //    list[k] = list[n];
+            //    list[n] = value;
+            //}
+
+
+            Random rnd = new Random();
+            int talihli;
+            string temp;
+            //random bir indexi, 0. indekse yerleştirdik ve böyle devam ederek random indexleri alıp 0,1,2,3,4.. şeklinde indexkere koyarak yenidedn dizdik(burda alttaki rnd sınırlarını belirlemek asıl olay)
+            //Bu sınırları da örneğin 0. indekse atama yapacaksak 0. indeks harici bir index numarası oluşturarak yaptık
+            for (int i = 0; i < kisiler.Count - 1; i++)
+            {
+                talihli = rnd.Next(i + 1, kisiler.Count);//(i+1 den eleman adedi kadar çünkü 0. index hedef index onu almadık eleman.Count çünkü random son sayısı almaz böylece max indexe kadar sayi üretir)
+                temp = kisiler[i];
+                kisiler[i] = kisiler[talihli];
+                kisiler[talihli] = temp;
+            }
+            KisileriListele();
+
+
+            lblSansli.Text = kisiler[0];
+
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)//Form kapanırken çalışan event (Closed eventi ise kapandıktan sonra çalışır)
+        {
+            File.WriteAllLines("veri.txt", kisiler);//projenin bulunduğu klasör => bin => debug klasörü altına kaydeder -- listedeki elemanları .txt formatlı içine yazdırdık
+        }
+
+        private void txtAd_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
+            {
+                e.SuppressKeyPress = true;//tuşa basınca çıkan sesi kapatır(dlılıng sesi)
+                btnEkle.PerformClick();//tuşa basma eventini çalıştırır
+            }
+        }
+
+        private void lstKisiler_KeyDown(object sender, KeyEventArgs e)
+        {
+            int sid = lstKisiler.SelectedIndex;
+
+            if (e.KeyCode == Keys.Delete && sid > -1)//hiçbiri seçili değilken hata vermemesi için(hiç biri seçili değilken index -1 olur)
+            {
+                kisiler.RemoveAt(sid);//ListBox.SelectedIndex seçili elemanın indexini int olarak döndürür, RemoveAt(int) bununla da bu indexi siliyoruz
+                KisileriListele();//silinen halini tekrar listeletip ekrandan kaybolmasını sağladık
+
+                if (sid < kisiler.Count)
+                    lstKisiler.SelectedIndex = sid;
+                else
+                    lstKisiler.SelectedIndex = sid - 1;
+            }
+        }
+
+        private void btnTop_Click(object sender, EventArgs e)
+        {
+            int sid = lstKisiler.SelectedIndex;
+            if (sid > 0)
+            {
+                Tasi(sid,0);
+                //string temp = kisiler[sid];
+                //kisiler.RemoveAt(sid);
+                //kisiler.Insert(0, temp);
+                //KisileriListele();
+                //lstKisiler.SelectedIndex = 0;
+                //lstKisiler.Focus();
+            }
+
+        }
+
+        private void btnBottom_Click(object sender, EventArgs e)
+        {
+            int sid = lstKisiler.SelectedIndex;
+            if (sid < kisiler.Count - 1 && sid > -1)
+            {
+                Tasi(sid,kisiler.Count-1);
+                //string temp = kisiler[sid];
+                //kisiler.RemoveAt(sid);
+                //kisiler.Insert(kisiler.Count,temp);
+                //KisileriListele();
+                //lstKisiler.SelectedIndex = kisiler.Count - 1;
+                //lstKisiler.Focus();
+            }
+        }
+
+        private void btnUp_Click(object sender, EventArgs e)
+        {
+            int sid = lstKisiler.SelectedIndex;
+            if (sid > 0)
+            {
+                Tasi(sid,sid-1);
+                //string temp = kisiler[sid];
+                //kisiler.RemoveAt(sid);
+                //kisiler.Insert(sid - 1, temp);
+                //KisileriListele();
+                //lstKisiler.SelectedIndex = sid - 1;
+                //lstKisiler.Focus();
+            }
+        }
+
+        private void btnDown_Click(object sender, EventArgs e)
+        {
+            int sid = lstKisiler.SelectedIndex;
+            if (sid > -1 && sid < kisiler.Count - 1)
+            {
+                Tasi(sid,sid+1);
+                //string temp = kisiler[sid];
+                //kisiler.RemoveAt(sid);
+                //kisiler.Insert(sid + 1, temp);
+                //KisileriListele();
+                //lstKisiler.SelectedIndex = sid + 1;
+                //lstKisiler.Focus();
+            }
+        }
+        void Tasi(int kaynakIndeks,int hedefIndex)//aşağı yukarı en üst en alt methodları benzer işlem yaptığı için refactoring ile daha temiz gözükmesini sağladık
+        {
+            
+            string temp = kisiler[kaynakIndeks];
+            kisiler.RemoveAt(kaynakIndeks);
+            kisiler.Insert(hedefIndex, temp);
+            KisileriListele();
+            lstKisiler.SelectedIndex = hedefIndex;
+            lstKisiler.Focus();
+        }
+
+        
+    }
+}
